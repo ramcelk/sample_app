7bc1a67935d7faffbe0eef88406afe26ae9e1680
Rogers Maradiaga <ramcelk@gmail.com>
2013-07-10 07:28:19 -0600

    Fin user REST

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ SampleApp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  config.assets.precompile += ['static_pages.css']
+   config.assets.precompile += ['sessions.css']
+
 end
